228c7a25b65d68d2c19eab2c8d99753b801c1144
EnricoNunes17 <enriconunes17@gmail.com>
2025-03-17 22:32:12 -0300

    Aula 5

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//for(let i = 1; i < 100; i += 2){
+  //  console.log(i)
+//}
+
+//let i = 5;
+//while(i<=500){
+    i++;
+    //i = i + 1;
+    //i +=5;
+
+//}
+
+//let num = prompt("Digite um número")
+//if(num<0){
+//    console.log("Número inválido!")
+//}
+//for(let i = num; i >=0;i-=1){
+//    console.log(i);
+//}
