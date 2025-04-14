72a4b9b1f7632135d195b286481267e278f92840
EnricoNunes17 <enriconunes17@gmail.com>
2025-04-14 22:32:05 +0000

    lab 06

--- LAB 06/lab.js
+++ LAB 06/lab.js
@@ -1,32 +1,50 @@
-let canvas = document.getElementById ('canvas')
-let ctx = canvas.getContext('2d')
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+
 
 let bola = {
-    x: 200,
-    y: 200,
-    raio: 50,
-    img: new Image(),
-    desenha: function(){
-        this.img.src = 'BOLA.PNG';
-        ctx.beginPath();
-        ctx.drawImage(this.img, this.x, this.y, 2*this.raio, 2*this.raio);
-        ctx.closePath();
-    }
+x: 250,
+y: 250,
+raio: 50,
+img: new Image(),
+desenha: function() {
+this.img.src = 'bola.png';
+ctx.beginPath();
+ctx.drawImage(this.img, this.x, this.y, 2*this.raio, 2*this.raio);
+ctx.closePath();
+}
 }
 
 function animacao(){
-    if(ret_1.x == 300-ret_1.largura){
+if (bola.x == 300) {
+bola.x = 300
+}
+
+ctx.clearRect(0,0,400,400)
+bola.desenha();
+requestAnimationFrame(animacao)
+}
+animacao();
+document.addEventListener('mousemove',function(evento){
+let rect = canvas.getBoundingClientRect();
+let x_mouse = evento.clientX - rect.left - 50;
+let y_mouse = evento.clientY - rect.top - 50;
+console.log(x_mouse,y_mouse);
+bola.x = x_mouse;
+bola.y = y_mouse;
+if (bola.x > 200){
+bola.x = 200
+}
+if (bola.x < 0){
+bola.x = 0
+}
+if (bola.y < 0){
+bola.y = 0
+}
+if (bola.y > 200){
+bola.y = 200
+}
 
 
-}
-}
-animacao()
-document.addEventListener('mousemove', function(evento){
-    let rect = canvas.getBoundingClientRect();
-    let x_mouse = evento.clientX - rect.left;
-    let y_mouse = evento.clientY - rect.top;
-    console.log(x_mouse,y_mouse);
-    
-    ret_3.x = x_mouse
-    ret_3.y = y_mouse
 })
