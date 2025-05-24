08dcfd9dfca7a57b6571cc145e3196c3d03771be
EnricoNunes17 <enriconunes17@gmail.com>
2025-05-23 23:09:50 -0300

    LAB10 PARTE 1

--- SERVIDOR/Servidor.js
+++ SERVIDOR/Servidor.js
@@ -1,58 +1,41 @@
+const http = require('http');
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-const server = express();
-const { MongoClient } = require('mongodb');
+
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
 
 const uri = 'mongodb+srv://dantasmilan23:L05ang3l35@enrico.pvyxi2g.mongodb.net/?retryWrites=true&w=majority&appName=Enrico';
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {useNewUrlParser: true});
 
-let db;
+var dbo = client.db("Lab10");
+var usuarios = dbo.collection("usuarios");
+var carros = dbo.collection("carros")
 
-// Conecta ao banco de dados
-client.connect()
-    .then(() => {
-        db = client.db('blogDB'); // Nome do banco de dados
-        console.log('Conectado ao MongoDB');
-    })
-    .catch(err => {
-        console.error('Erro ao conectar ao MongoDB:', err);
-    });
-
-// Middleware para servir arquivos estáticos e processar requisições POST
-server.use(express.static(path.join(__dirname, 'public')));
-server.use(bodyParser.urlencoded({ extended: true }));
-
-// Configuração do EJS para renderizar páginas dinâmicas
-server.set('view engine', 'ejs');
-server.set('views', path.join(__dirname, 'views'));
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 // Rotas principais
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.redirect('Lab1/projects.html');
 });
 
-server.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
 
-server.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Lab1/projects.html'));
-});
-
-server.get('/cadastra', (req, res) => {
+app.get('/cadastra', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'LAB08/Cadastro.html'));
 });
 
-server.get('/login', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'LAB08/login.html'));
 });
 
-// Simula um banco de dados em memória para usuários
-const users = [];
-
 // Rota para processar o cadastro de usuário
-server.post('/cadastra', (req, res) => {
+app.post('/cadastra', (req, res) => {
     const { username, password } = req.body;
     if (users.find(user => user.username === username)) {
         return res.send('Usuário já cadastrado!');
@@ -62,7 +45,7 @@ server.post('/cadastra', (req, res) => {
 });
 
 // Rota para processar o login
-server.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const user = users.find(user => user.username === username && user.password === password);
     const status = user ? 'Sucesso' : 'Falha';
@@ -70,7 +53,7 @@ server.post('/login', (req, res) => {
 });
 
 // Página de blog renderizada dinamicamente com posts do MongoDB
-server.get('/blog', async (req, res) => {
+app.get('/blog', async (req, res) => {
     try {
         const collection = db.collection('posts');
         const posts = await collection.find().toArray();
@@ -82,12 +65,12 @@ server.get('/blog', async (req, res) => {
 });
 
 // Serve a página cadastrar_post.html
-// server.get('/cadastrar_post', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'LAB09/cadastrar_post.html'));
-// });
+app.get('/cadastrar_post', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'LAB09/cadastrar_post.html'));
+});
 
 // Rota para cadastrar um novo post no banco de dados
-server.post('/cadastrar', async (req, res) => {
+app.post('/cadastrar', async (req, res) => {
     const { titulo, resumo, conteudo } = req.body;
 
     try {
@@ -101,7 +84,7 @@ server.post('/cadastrar', async (req, res) => {
 });
 
 // Inicia o servidor na porta 80 (recomendada para ambiente local)
-server.listen(80, () => {
+app.listen(80, () => {
     console.log('Servidor rodando na porta 80');
     console.log('Acesse pelo navegador: http://localhost:80/');
 });
