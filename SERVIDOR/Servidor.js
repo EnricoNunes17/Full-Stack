8be7fd9aabe6f5dd7a1d90ed8b11d5ab7f19c772
EnricoNunes17 <enriconunes17@gmail.com>
2025-05-25 20:10:12 -0300

    LAB 10 FINALE

--- SERVIDOR/Servidor.js
+++ SERVIDOR/Servidor.js
@@ -1,90 +1,248 @@
+require("colors");
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const mongodb = require("mongodb");
 
-var mongodb = require("mongodb");
-
+const app = express();
 const MongoClient = mongodb.MongoClient;
 
 const uri = 'mongodb+srv://dantasmilan23:L05ang3l35@enrico.pvyxi2g.mongodb.net/?retryWrites=true&w=majority&appName=Enrico';
-const client = new MongoClient(uri, {useNewUrlParser: true});
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-var dbo = client.db("Lab10");
-var usuarios = dbo.collection("usuarios");
-var carros = dbo.collection("carros")
+const dbo = client.db("exemplo_bd");
+const usuarios = dbo.collection("usuarios");
 
-var app = express();
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
+const dboo = client.db("exemplo_bd2");
+const posts = dboo.collection("posts");
+
+const dbbo = client.db("LAB10");
+const user = dbbo.collection("usuarios");
+const carros = dbbo.collection("carros");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.set('views', './views');
+
+const server = http.createServer(app);
+server.listen(80);
+console.log("Servidor rodando na porta 80");
+
 
 // Rotas principais
 app.get('/', (req, res) => {
-    res.redirect('Lab1/projects.html');
+  res.redirect('Lab1/projects.html');
 });
 
-
 app.get('/cadastra', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'LAB08/Cadastro.html'));
+  res.redirect('public/LAB08/Cadastro.html');
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'LAB08/login.html'));
+  res.redirect('public/LAB08/login.html');
 });
 
-// Rota para processar o cadastro de usuário
-app.post('/cadastra', (req, res) => {
-    const { username, password } = req.body;
-    if (users.find(user => user.username === username)) {
-        return res.send('Usuário já cadastrado!');
+app.get('/blog', (req, res) => {
+  posts.find().toArray((err, resultados) => {
+    if (err) res.send('Erro ao buscar os posts');
+    else res.render('blog.ejs', { posts: resultados });
+  });
+});
+
+app.post('/cad_blog', (req, res) => {
+  res.redirect('lab1/lab_09/cadastrar_post.html');
+});
+
+app.post('/inicio', (req, res) => {
+  res.redirect('aula_04/index.html');
+});
+
+// Cadastro via query
+app.get('/cadastrar', (req, res) => {
+  const { nome, email, senha, nascimento } = req.query;
+  res.render('resposta.ejs', {
+    mensagem: "Cadastro finalizado com sucesso!",
+    usuario: nome,
+    login: email,
+    idade: nascimento
+  });
+});
+
+// Cadastro via body
+app.post('/cadast', (req, res) => {
+  const { nome, email, senha, nascimento } = req.body;
+  const data = { db_nome: nome, db_email: email, db_senha: senha, db_nascimento: nascimento };
+  usuarios.insertOne(data, err => {
+    if (err) res.render('respost.ejs', { mensagem: "Erro ao cadastrar usuário!", usuario: nome, login: email });
+    else res.render('respost.ejs', { mensagem: "Cadastro finalizado com sucesso!", usuario: nome, login: email, idade: nascimento });
+  });
+});
+
+app.post('/log', (req, res) => {
+  const { email, senha } = req.body;
+  const data = { db_email: email, db_senha: senha };
+  usuarios.find(data).toArray((err, items) => {
+    if (err) res.render('respost_log.ejs', { mensagem: "Erro ao logar usuário!", login: email });
+    else if (items.length == 0) res.render('respost_log.ejs', { mensagem: "Usuário/senha não encontrado!", login: email });
+    else res.render('respost_log.ejs', { mensagem: "Login realizado com sucesso!", login: email });
+  });
+});
+//CRUD
+app.post("/cadastrar_usuario", (req, res) => {
+  const data = { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha };
+  usuarios.insertOne(data, err => {
+    if (err) res.render('resposta_usuario', { resposta: "Erro ao cadastrar usuário!" });
+    else res.render('resposta_usuario', { resposta: "Usuário cadastrado com sucesso!" });
+  });
+});
+
+app.post("/logar_usuario", (req, res) => {
+  const data = { db_login: req.body.login, db_senha: req.body.senha };
+  usuarios.find(data).toArray((err, items) => {
+    if (err) res.render('resposta_usuario', { resposta: "Erro ao logar usuário!" });
+    else if (items.length == 0) res.render('resposta_usuario', { resposta: "Usuário/senha não encontrado!" });
+    else res.render('resposta_usuario', { resposta: "Usuário logado com sucesso!" });
+  });
+});
+
+app.post("/atualizar_usuario", (req, res) => {
+  const data = { db_login: req.body.login, db_senha: req.body.senha };
+  const newData = { $set: { db_senha: req.body.novasenha } };
+  usuarios.updateOne(data, newData, (err, result) => {
+    if (err) res.render('resposta_usuario', { resposta: "Erro ao atualizar usuário!" });
+    else if (result.modifiedCount == 0) res.render('resposta_usuario', { resposta: "Usuário/senha não encontrado!" });
+    else res.render('resposta_usuario', { resposta: "Usuário atualizado com sucesso!" });
+  });
+});
+
+app.post("/remover_usuario", (req, res) => {
+  const data = { db_login: req.body.login, db_senha: req.body.senha };
+  usuarios.deleteOne(data, (err, result) => {
+    if (err) res.render('resposta_usuario', { resposta: "Erro ao remover usuário!" });
+    else if (result.deletedCount == 0) res.render('resposta_usuario', { resposta: "Usuário/senha não encontrado!" });
+    else res.render('resposta_usuario', { resposta: "Usuário removido com sucesso!" });
+  });
+});
+// Lab 09
+app.post('/cadpost', function(requisicao, resposta){
+    let titulo = requisicao.body.titulo;
+    let resumo = requisicao.body.resumo;
+    let conteudo = requisicao.body.conteudo;
+
+    let data = {db_titulo: titulo, db_resumo: resumo,db_conteudo: conteudo}
+    
+    posts.insertOne(data, function (err) {
+          if (err) {
+            resposta.render('resposta.ejs', 
+                {mensagem: "Não Cadastrato!"})
+          }else {
+            resposta.render('resposta.ejs', 
+                {mensagem: "Cadastro"})       
+          };
+        });
+       
+      });
+
+app.get('/listarsposts.ejs', function(requisicao, resposta) {
+    posts.find().toArray(function(err, resultados) {
+        if (err) {
+            resposta.send('Erro ao buscar os posts');
+        } else {
+            resposta.render('listarsposts.ejs', { posts: resultados });
+        }
+    });
+});
+
+// lab10
+app.get('/Cadastrar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/lab1/LAB10/usuarios/cadastro.html'));
+});
+
+app.get('/logar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/lab1/LAB10/usuarios/login.html'));
+});
+
+app.post('/Cadastrar', (req, res) => {
+  const { nome, email, senha, nascimento } = req.body;
+  const data = { db_nome: nome, db_email: email, db_senha: senha, db_nascimento: nascimento };
+
+  user.insertOne(data, (err) => {
+    if (err) {
+      res.render('resposta_cadastro.ejs', { mensagem: "Erro ao cadastrar usuário!", login: email });
+    } else {
+      res.render('resposta_cadastro.ejs', { mensagem: "Cadastro realizado com sucesso!", login: email });
     }
-    users.push({ username, password });
-    res.send('Cadastro realizado com sucesso! <a href="/login">Ir para Login</a>');
+  });
 });
 
-// Rota para processar o login
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(user => user.username === username && user.password === password);
-    const status = user ? 'Sucesso' : 'Falha';
-    res.render('resposta', { username, status });
+app.post('/logar', (req, res) => {
+  const { email, senha } = req.body;
+  const data = { db_email: email, db_senha: senha };
+
+  user.find(data).toArray((err, items) => {
+    if (err) res.render('resposta_cadastro.ejs', { mensagem: "Erro ao logar usuário!", login: email });
+    else if (items.length === 0) res.render('resposta_cadastro.ejs', { mensagem: "Usuário/senha não encontrado!", login: email });
+    else res.redirect('/pg_principal');
+  });
 });
 
-// Página de blog renderizada dinamicamente com posts do MongoDB
-app.get('/blog', async (req, res) => {
-    try {
-        const collection = db.collection('posts');
-        const posts = await collection.find().toArray();
-        res.render('blog', { posts });
-    } catch (err) {
-        console.error('Erro ao buscar posts:', err);
-        res.status(500).send('Erro ao carregar posts.');
+app.get('/pg_principal', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/lab1/LAB10/pg_principal.html'));
+});
+
+// Cadastro de carro
+app.get('/cadast_carro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/lab1/LAB10/carros/cadast_carro.html'));
+});
+
+app.post('/cadast_carro', (req, res) => {
+  const { marca, modelo, ano, qtd } = req.body;
+  const data = { db_marca: marca, db_modelo: modelo, db_ano: ano, db_qtd: parseInt(qtd) };
+
+  carros.insertOne(data, (err) => {
+    if (err) res.render('resposta_cadast_carro.ejs', { mensagem: "Erro ao cadastrar carro!", marca_c: marca, modelo_c: modelo });
+    else res.render('resposta_cadast_carro.ejs', { mensagem: "Cadastro realizado com sucesso!", marca_c: marca, modelo_c: modelo, ano_c: ano, qtd_c: qtd });
+  });
+});
+
+// Listar carros
+app.get('/listar_carros', (req, res) => {
+  carros.find().toArray((err, lista) => {
+    if (err) res.send("Erro ao listar carros.");
+    else res.render('listar_carros.ejs', { carros: lista });
+  });
+});
+
+// Vender carro 
+app.post('/vender_carro', (req, res) => {
+  const id = new mongodb.ObjectId(req.body.id);
+  carros.findOne({ _id: id }, (err, carro) => {
+    if (err || !carro) return res.send("Carro não encontrado.");
+
+    if (carro.db_qtd <= 0) {
+      res.send("Esgotado");
+    } else {
+      carros.updateOne({ _id: id }, { $inc: { db_qtd: -1 } }, () => {
+        res.redirect('/listar_carros');
+      });
     }
+  });
 });
 
-// Serve a página cadastrar_post.html
-app.get('/cadastrar_post', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'LAB09/cadastrar_post.html'));
+// Remover carro
+app.post('/remover_carro', (req, res) => {
+  const id = new mongodb.ObjectId(req.body.id);
+  carros.deleteOne({ _id: id }, () => res.redirect('/listar_carros'));
 });
 
-// Rota para cadastrar um novo post no banco de dados
-app.post('/cadastrar', async (req, res) => {
-    const { titulo, resumo, conteudo } = req.body;
+// Atualizar carro (exemplo com qtd)
+app.post('/atualizar_carro', (req, res) => {
+  const id = new mongodb.ObjectId(req.body.id);
+  const novaQtd = parseInt(req.body.nova_qtd);
 
-    try {
-        const collection = db.collection('posts');
-        await collection.insertOne({ titulo, resumo, conteudo });
-        res.redirect('/blog'); // Corrigido
-    } catch (err) {
-        console.error('Erro ao cadastrar post:', err);
-        res.status(500).send('Erro ao cadastrar post.');
-    }
+  carros.updateOne({ _id: id }, { $set: { db_qtd: novaQtd } }, () => res.redirect('/listar_carros'));
 });
 
-// Inicia o servidor na porta 80 (recomendada para ambiente local)
-app.listen(80, () => {
-    console.log('Servidor rodando na porta 80');
-    console.log('Acesse pelo navegador: http://localhost:80/');
-});
